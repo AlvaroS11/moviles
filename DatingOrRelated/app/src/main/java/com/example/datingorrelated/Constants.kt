2562174d9ad0f950774c03504d181ac0daa74aa8
AlvaroS11 <alvarogarcia111@gmail.com>
2022-10-25 20:18:34 +0200

    added questions

--- DatingOrRelated/app/src/main/java/com/example/datingorrelated/Constants.kt
+++ DatingOrRelated/app/src/main/java/com/example/datingorrelated/Constants.kt
@@ -9,9 +9,35 @@ object Constants {
         val q2 = Question(1, "Zeus & Metis", R.drawable.zeus, R.drawable.metis, "Dating")
         val q3 = Question(1, "Zeus & Hestia", R.drawable.zeus, R.drawable.hestia, "Related")
 
+        val q4 = Question(1, "Gaia & Uranus", R.drawable.gaia, R.drawable.zeus, "Both")
+        val q5 = Question(1, "Zeus & Rhea", R.drawable.zeus, R.drawable.rhea, "Both")
+        val q6 = Question(1, "Chronos & Rhea", R.drawable.chronos, R.drawable.rhea, "Both")
+        val q7 = Question(1, "Zeus & Demeter", R.drawable.zeus, R.drawable.demeter, "Both")
+
+
+        val q8 = Question(1, "Zeus & Eris", R.drawable.zeus, R.drawable.eris, "Dating")
+        val q9 = Question(1, "Zeus & Mnemosine", R.drawable.zeus, R.drawable.mnemosine, "Dating")
+        val q11 = Question(1, "Zeus & Ganímedes", R.drawable.zeus, R.drawable.ganimede, "Dating")
+
+        val q12 = Question(1, "Zeus & Poseidon", R.drawable.zeus, R.drawable.poseidon, "Related")
+        val q13 = Question(1, "Hera & Hades", R.drawable.hera, R.drawable.hades, "Related")
+        val q14 = Question(1, "Gaia & Eros", R.drawable.gaia, R.drawable.eros, "Related")
+
         questionsList.add(q1)
         questionsList.add(q2)
         questionsList.add(q3)
+        questionsList.add(q4)
+        questionsList.add(q5)
+        questionsList.add(q6)
+        questionsList.add(q7)
+        questionsList.add(q8)
+        questionsList.add(q9)
+        questionsList.add(q11)
+        questionsList.add(q12)
+        questionsList.add(q13)
+        questionsList.add(q14)
+
+
         return questionsList
     }
 }
